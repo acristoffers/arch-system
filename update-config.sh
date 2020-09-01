95446a47c1c09ccab9224ed168af3f918e5b10b4
Álan Crístoffer <acristoffers@gmail.com>
2020-09-01 17:44:05 -0300

    Adds missing -r to cp commands.

--- update-config.sh
+++ update-config.sh
@@ -2,11 +2,11 @@
 
 rm -r ~/.doom.d ~/.xmonad ~/.config/xmobar
 cp -r dotfiles/doom.d/ ~/.doom.d
-cp dotfiles/vimrc ~/.vimrc
-cp dotfiles/tmux.conf ~/.tmux.conf
-cp dotfiles/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-cp dotfiles/xmonad/ ~/.xmonad
-cp dotfiles/config/xmobar/ ~/.config/xmobar
+cp -r dotfiles/vimrc ~/.vimrc
+cp -r dotfiles/tmux.conf ~/.tmux.conf
+cp -r dotfiles/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp -r dotfiles/xmonad/ ~/.xmonad
+cp -r dotfiles/config/xmobar/ ~/.config/xmobar
 
 ~/.emacs.d/bin/doom -y sync
 ~/.emacs.d/bin/doom -y upgrade
