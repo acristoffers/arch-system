6b6464322f20d3f3af2f41f3887bf0eee245dd26
Álan Crístoffer <acristoffers@gmail.com>
2020-09-01 17:36:42 -0300

    Adds config udpate script.

---
+++ update-config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -r ~/.doom.d ~/.xmonad ~/.config/xmobar
+cp -r dotfiles/doom.d/ ~/.doom.d
+cp dotfiles/vimrc ~/.vimrc
+cp dotfiles/tmux.conf ~/.tmux.conf
+cp dotfiles/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp dotfiles/xmonad/ ~/.xmonad
+cp dotfiles/config/xmobar/ ~/.config/xmobar
+
+~/.emacs.d/bin/doom -y sync
+~/.emacs.d/bin/doom -y upgrade
+nvim --headless +PlugUpgrade +qa
+nvim --headless +PlugUpdate +qa
+nvim --headless +CocUpdateSync +qa
