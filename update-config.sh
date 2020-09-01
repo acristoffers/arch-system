de82208a209f56e46b0e995475e966b1be56f85e
Álan Crístoffer <acristoffers@gmail.com>
2020-09-01 17:52:05 -0300

    Reapply promptline patch after update.

--- update-config.sh
+++ update-config.sh
@@ -15,6 +15,12 @@ nvim --headless +PlugUpdate +qa
 nvim --headless +CocUpdateSync +qa
 nvim --headless +'PromptlineSnapshot! ~/.promptline.sh airline' +qa
 
+nvim ~/.promptline.sh -c 'argdo /truncation' \
+                      -c 'argdo normal di"' \
+                      -c 'argdo /dir_sep' \
+                      -c 'argdo normal di"' \
+                      -c 'wq'
+
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 fish -c "fisher add dracula/fish"
 
