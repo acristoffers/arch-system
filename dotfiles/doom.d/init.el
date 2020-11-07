c1ae81df39db6e06d02d622c9c232ef76f55a18f
Álan Crístoffer <acristoffers@gmail.com>
2020-11-07 19:45:05 -0300

    Adds journal support.

--- dotfiles/doom.d/init.el
+++ dotfiles/doom.d/init.el
@@ -146,7 +146,11 @@
        nim                            ; python + lisp at the speed of c
        nix                            ; I hereby declare "nix geht mehr!"
        ocaml                          ; an objective camel
-       (org +dragndrop +hugo +pretty) ; organize your plain life in plain text
+       (org
+        +dragndrop
+        +journal
+        +hugo
+        +pretty)                      ; organize your plain life in plain text
        ;;php                          ; perl's insecure younger brother
        ;;plantuml                     ; diagrams for confusing people more
        ;;purescript                   ; javascript, but functional
