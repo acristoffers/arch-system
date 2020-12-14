f06b552edf6cba985c40c50cde018326bcd159ed
Álan Crístoffer <acristoffers@gmail.com>
2020-12-14 20:16:54 -0300

    Fix xmobar folder structure.

--- dotfiles/config/xmobar/xmobarrc0.hs
+++ dotfiles/config/xmobar/xmobarrc0.hs
@@ -22,12 +22,28 @@ Config { font    = "xft:Inconsolata Nerd Font:Regular:pixelsize=14:antialias=tru
                       -- Prints out the left side items such as workspaces, layout, etc.
                       -- The workspaces are 'clickable' in my configs.
                     , Run UnsafeStdinReader
+                    , Run Com "bash" ["/home/alan/.config/xmobar/volume.sh"] "volume" 1
+                    , Run Wireless "wlan0" [ "-t", "\xf1eb  <essid>" ] 10
+                    -- network activity monitor (dynamic interface resolution)
+                    , Run DynNetwork [ "--template" , "\xf700  <dev>"
+                             , "--Low"      , "1000"       -- units: B/s
+                             , "--High"     , "5000"       -- units: B/s
+                             , "--low"      , "darkgreen"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkred"
+                             ] 10
                     ]
        , sepChar = "%"
        , alignSep = "}{"
        , template = " <icon=haskell_20.xpm/> \
                     \<fc=#666666>|</fc>\
                     \ %UnsafeStdinReader% }{ \
+                    \<fc=#c3e88d> %volume% </fc>\
+                    \<fc=#666666>|</fc>\
+                    \<fc=#c3e88d> %dynnetwork% </fc>\
+                    \<fc=#666666>|</fc>\
+                    \<fc=#c3e88d> %wlan0wi% </fc>\
+                    \<fc=#666666>|</fc>\
                     \<fc=#c3e88d> %battery% </fc>\
                     \<fc=#666666>|</fc>\
                     \<fc=#8BE9FD> %date% </fc>\
